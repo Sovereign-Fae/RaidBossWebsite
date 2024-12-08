501342370f64102914655fd1b5805f4ac76035ee
Sovereign-Fae <jacobrandomz@gmail.com>
2024-12-08 00:36:40 -0600

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// Import Firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
+
+// Your Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBvX9PUPDZbjrFciimBZ2sX0j7jbLZAhg",
+    authDomain: "raidbossgame-59ed1.firebaseapp.com",
+    databaseURL: "https://raidbossgame-59ed1-default-rtdb.firebaseio.com",
+    projectId: "raidbossgame-59ed1",
+    storageBucket: "raidbossgame-59ed1.appspot.com",
+    messagingSenderId: "692473703930",
+    appId: "1:692473703930:web:b89bac49b4c9a30c804843",
+    measurementId: "G-JGG8BQEL92"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// Test writing to Firebase
+const testRef = ref(db, "test/");
+set(testRef, { message: "Hello, Firebase!" });
+
+
 // List of boss images
 const bossImages = [
     "boss1.jpg",
